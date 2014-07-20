7d5cd9a785744dd9f98490ee14d88535a6140750
Pascal Duez <pascal@duez.me>
2014-07-20 18:29:40 +0200

    chore(test): moved from mocha to tape

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,32 +2,19 @@
 
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
-var mocha = require('gulp-mocha');
+var shell = require('gulp-shell')
 var clean = require('gulp-clean');
 var gutil = require('gulp-util');
+var runSequence = require('run-sequence');
 var sassdoc = require('./');
 
-
-gulp.task('mocha', ['sassdoc'], function () {
-  return gulp
-    .src('test/*-test.js', { read: false })
-    .pipe(mocha({ reporter: 'spec' }))
-    .on('error', gutil.log);
-});
-
+console.log(require.resolve('tape'));
 
 gulp.task('lint', function () {
   return gulp
     .src(['tasks/*js', 'test/*.js'])
     .pipe(eslint())
     .pipe(eslint.format());
-});
-
-
-gulp.task('clean', function () {
-  return gulp
-    .src('test/docs', { read: false })
-    .pipe(clean());
 });
 
 
@@ -42,7 +29,7 @@ gulp.task('sassdoc_config', function () {
 });
 
 
-gulp.task('sassdoc', function () {
+gulp.task('sassdoc_options', function () {
   return gulp
     .src('test/fixture')
     .pipe(sassdoc({
@@ -58,4 +45,26 @@ gulp.task('sassdoc', function () {
 });
 
 
-gulp.task('test', ['lint', 'sassdoc', 'mocha', 'clean']);
+gulp.task('tape', shell.task([
+  'node test/*-test.js | faucet',
+]));
+
+
+gulp.task('clean', function () {
+  return gulp
+    .src('test/docs', { read: false })
+    .pipe(clean());
+});
+
+
+gulp.task('test', function () {
+  runSequence(
+    'lint',
+    'sassdoc_config',
+    'tape',
+    'clean',
+    'sassdoc_options',
+    'tape',
+    'clean'
+  );
+});
